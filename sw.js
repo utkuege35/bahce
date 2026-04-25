d521595271a17b3e6de81b76149d3c3a7326c5b9
utkuege35 <utku1ege1@gmail.com>
2026-04-25 14:47:55 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,24 +1,18 @@
-const CACHE = 'bahce-v1';
-const ASSETS = [
-  '/bahce/',
-  '/bahce/index.html',
-  '/bahce/manifest.json'
-];
-
+// Service Worker - Cache devre dışı, her zaman güncel kod
 self.addEventListener('install', e => {
-  e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
   self.skipWaiting();
 });
 
 self.addEventListener('activate', e => {
-  e.waitUntil(caches.keys().then(keys =>
-    Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
-  ));
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.map(k => caches.delete(k)))
+    )
+  );
   self.clients.claim();
 });
 
 self.addEventListener('fetch', e => {
-  e.respondWith(
-    fetch(e.request).catch(() => caches.match(e.request))
-  );
+  // Cache kullanma, her zaman network'ten al
+  e.respondWith(fetch(e.request));
 });
